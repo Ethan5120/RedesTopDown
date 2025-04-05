1bd5f8d19e77e978b748684f744440fb8ffde4da
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2025-04-04 19:49:39 -0600

    Finalizacion Menu Inicial

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
